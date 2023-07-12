857ff2638f8159d5606d5746d1dc33b17da65e8b
Tzui-wang <tzui.rita@gmail.com>
2023-07-12 16:57:27 +0800

    Wednesday

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-var hamburgerStatus=false;
+var hamburgerStatus = false;
 
 function hamburgerClicked() {
     var menuVerticle = document.getElementById('menu-verticle');
     var hamburgerImg = document.getElementById('hamburger-img');
-    if (hamburgerStatus==false){
+    if (hamburgerStatus == false) {
         menuVerticle.style.display = 'block';
         hamburgerImg.src = 'img/hamburger-x.png';
-        hamburgerStatus=true;
+        hamburgerStatus = true;
     }
-    else{
+    else {
         menuVerticle.style.display = 'none';
         hamburgerImg.src = 'img/hamburger-full.png';
-        hamburgerStatus=false;
+        hamburgerStatus = false;
     }
 }
+
+
+
+// var x = document.getElementById('btn1');
+//                 alert(x);
+window.onload = function () {
+    var contactbtn = document.getElementById("to-contact-btn");
+
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            contactbtn.style.display = "block";
+        } else {
+            contactbtn.style.display = "none";
+        }
+    }
+}
+
